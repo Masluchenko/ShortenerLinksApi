2b1726193527df02da1cca6341a047a19221908a
Маслюченко Никита Сергеевич <vivat404@IC.local>
2026-06-17 18:10:06 +0300

    add jwt create and get

--- pkg/middleware/auth.go
+++ pkg/middleware/auth.go
@@ -3,12 +3,14 @@ package middleware
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func IsAuthed(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		athedHeader := r.Header.Get("Authorization")
+		token := strings.TrimPrefix(athedHeader, "Bearer ")
+		fmt.Println(token)
 		next.ServeHTTP(w, r)
-		fmt.Println()
 	})
 }
